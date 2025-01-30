88c202126637e2c3f0c6b3c3aee0c77c13e1f786
Pricecito <matias.gutierrez.1254@gmail.com>
2025-01-29 20:15:18 -0500

    Modelo Pedidos

---
+++ src/main/java/com/api/productos/model/EstadosEnum.java
@@ -0,0 +1,7 @@
+package com.api.productos.model;
+
+public enum EstadosEnum {
+    EMPAQUETAMIENTO,
+    EN_CAMINO,
+    ENTREGADO
+}
--- src/main/java/com/api/productos/model/Pedidos.java
+++ src/main/java/com/api/productos/model/Pedidos.java
@@ -1,5 +1,33 @@
 package com.api.productos.model;
 
-public class Pedidos {
+import java.time.LocalDate;
+import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedidos {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private UUID id;
+
+    private Double total;
+
+    @Enumerated(EnumType.STRING)
+    private EstadosEnum estado;
+
+    private LocalDate fecha_Creacion;
 }
