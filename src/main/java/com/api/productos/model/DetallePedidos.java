2f55bed3c653793b15645a4f3a3124d68758ff6d
Pricecito <matias.gutierrez.1254@gmail.com>
2025-01-29 20:32:54 -0500

    Add relationships to DetallePedidos model with Pedidos and Productos

--- src/main/java/com/api/productos/model/DetallePedidos.java
+++ src/main/java/com/api/productos/model/DetallePedidos.java
@@ -1,10 +1,13 @@
 package com.api.productos.model;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,10 +23,16 @@ public class DetallePedidos {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     private Long cantidad;
 
     private Double subtotal;
 
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "pedido_id")
+    private Pedidos pedido;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id")
+    private Productos producto;
 
 }
