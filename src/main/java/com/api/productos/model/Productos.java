ff76c45975416a1ddadda4909e08dbfa1fa7c459
Pricecito <matias.gutierrez.1254@gmail.com>
2025-01-29 20:30:34 -0500

    Add Categorias relationship to Productos model with JPA annotations

--- src/main/java/com/api/productos/model/Productos.java
+++ src/main/java/com/api/productos/model/Productos.java
@@ -3,10 +3,14 @@ package com.api.productos.model;
 import java.io.Serializable;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -17,17 +21,22 @@ import lombok.*;
 @Table(name = "productos")
 public class Productos implements Serializable {
     @Id
-    @GeneratedValue(strategy=GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String nombre;
 
+    @Column(nullable = true)
     private String descripcion;
 
+    @Column(nullable = false)
     private Double precio;
 
+    @Column(nullable = false)
     private Integer stock;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Categorias categoria;
 
-    
 }
