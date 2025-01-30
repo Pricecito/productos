57f40004fa5454cd78094eca39d0cf7bbf68f641
Pricecito <matias.gutierrez.1254@gmail.com>
2025-01-29 20:05:59 -0500

    Modelo Productos

---
+++ src/main/java/com/api/productos/model/Productos.java
@@ -0,0 +1,33 @@
+package com.api.productos.model;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "productos")
+public class Productos implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    private UUID id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private double precio;
+
+    private int stock;
+
+    
+    
+}
