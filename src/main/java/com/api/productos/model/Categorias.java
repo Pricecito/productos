c83ba914e3492589f2c56927dc85326fa6c50b7c
Pricecito <matias.gutierrez.1254@gmail.com>
2025-02-02 19:49:04 -0500

    Relaciones terminadas

--- src/main/java/com/api/productos/model/Categorias.java
+++ src/main/java/com/api/productos/model/Categorias.java
@@ -1,5 +1,6 @@
 package com.api.productos.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Categorias {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String nombre;
 
 }
--- src/main/java/com/api/productos/model/Productos.java
+++ src/main/java/com/api/productos/model/Productos.java
@@ -10,6 +10,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.*;
@@ -37,6 +38,7 @@ public class Productos implements Serializable {
     private Integer stock;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "categoria_id")
     private Categorias categoria;
 
 }
