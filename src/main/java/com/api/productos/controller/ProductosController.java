82a27a660bb8e405698610db629e3a32e3e4b89c
Pricecito <matias.gutierrez.1254@gmail.com>
2025-02-09 17:06:11 -0500

    Patch y Delete Mapping

--- src/main/java/com/api/productos/controller/ProductosController.java
+++ src/main/java/com/api/productos/controller/ProductosController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -34,16 +36,40 @@ public class ProductosController {
 
     @PostMapping
     public ResponseEntity<Productos> save(@RequestBody Productos producto){
-        return ResponseEntity.ok(service.save(producto));
+        URI location= URI.create(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(producto.getId()).toUriString());
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping
     public ResponseEntity<Productos> update(@RequestBody Productos producto){
         Productos p = service.findById(producto.getId()).get();
-        if(Objects.isNull(p)){
-            return ResponseEntity.notFound().build();
+        if(Objects.nonNull(p)){
+            service.save(p);
+            return ResponseEntity.noContent().build();
         }
-        service.save(p);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<Productos> updatePartial(@RequestBody Productos producto){
+        Productos p = service.findById(producto.getId()).get();
+        if(Objects.nonNull(p)){
+            if(Objects.nonNull(producto.getNombre()))p.setNombre(producto.getNombre());
+            if(Objects.nonNull(producto.getDescripcion()))p.setDescripcion(producto.getDescripcion());
+            if(Objects.nonNull(producto.getPrecio()))p.setPrecio(producto.getPrecio());
+            if(Objects.nonNull(producto.getStock()))p.setStock(producto.getStock());
+            service.save(p);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Productos> delete(@PathVariable("id") UUID id){
+        Productos p = service.findById(id).get();
+        if(Objects.nonNull(p)){
+            service.deleteById(p.getId());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
