832e912dc139051e8fb8d084162c5af0ff0f3bd5
Pricecito <matias.gutierrez.1254@gmail.com>
2025-02-12 18:55:12 -0500

    PoductosDTO

---
+++ src/main/java/com/api/productos/dto/ProductosDTO.java
@@ -0,0 +1,19 @@
+package com.api.productos.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductosDTO {
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+}
