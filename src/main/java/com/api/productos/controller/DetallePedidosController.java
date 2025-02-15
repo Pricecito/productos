67154dd19145a9508bc84a348c4b30efd0430164
Pricecito <matias.gutierrez.1254@gmail.com>
2025-02-14 20:16:09 -0500

    Post

--- src/main/java/com/api/productos/controller/DetallePedidosController.java
+++ src/main/java/com/api/productos/controller/DetallePedidosController.java
@@ -6,9 +6,10 @@ import com.api.productos.service.DetallePedidosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +21,11 @@ public class DetallePedidosController {
 
     @GetMapping
     public ResponseEntity<List<DetallePedidosDTO>> findAll() {
-        List<DetallePedidos> list=service.findAll();
-        if(list.isEmpty()){
+        List<DetallePedidos> list = service.findAll();
+        if (list.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-        List<DetallePedidosDTO> listDto=list.stream().map( detalles -> DetallePedidosDTO.builder()
+        List<DetallePedidosDTO> listDto = list.stream().map(detalles -> DetallePedidosDTO.builder()
                 .id(detalles.getId())
                 .cantidad(detalles.getCantidad())
                 .subtotal(detalles.getSubtotal())
@@ -33,6 +34,47 @@ public class DetallePedidosController {
                 .build()).toList();
         return ResponseEntity.ok(listDto);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetallePedidosDTO> findById(@PathVariable Long id) {
+        DetallePedidos detalle = service.findById(id).get();
+        DetallePedidosDTO dto = DetallePedidosDTO.builder()
+                .id(detalle.getId())
+                .cantidad(detalle.getCantidad())
+                .subtotal(detalle.getSubtotal())
+                .producto(detalle.getProducto().getNombre())
+                .pedidoId(detalle.getPedido().getId())
+                .build();
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<DetallePedidos> create(@RequestBody DetallePedidos dto) {
+        DetallePedidos detalle = service.save(dto);
+        URI location = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUriString());
+        return ResponseEntity.created(location).build();
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DetallePedidosDTO> update(@PathVariable Long id, @RequestBody DetallePedidosDTO dto) {
+        DetallePedidos detalle = service.update(id, dto);
+        DetallePedidosDTO resultDto = DetallePedidosDTO.builder()
+                .id(detalle.getId())
+                .cantidad(detalle.getCantidad())
+                .subtotal(detalle.getSubtotal())
+                .producto(detalle.getProducto().getNombre())
+                .pedidoId(detalle.getPedido().getId())
+                .build();
+        return ResponseEntity.ok(resultDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
